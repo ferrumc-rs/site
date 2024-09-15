206767b3cd8b15a51d337b8648d98ea8e333242c
cookiejul <181127962+cookiejul@users.noreply.github.com>
2024-09-15 16:49:32 +0800

    fix: hero typewriter

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,27 +1,30 @@
-import { Github } from "lucide-react";
 import { TypeAnimation } from "react-type-animation";
 import { GitHubBtn } from "./github-btn";
 
 export const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-start h-[calc(100vh-80px)] pt-[80px]">
+    <div className="flex flex-col items-center justify-start min-h-[calc(100vh-80px)] pt-[80px]">
       <h1 className="text-white text-center font-roboto text-[72px] font-extrabold leading-[110%] max-w-[994px]">
-        <span className="pr-6">A</span>
         <TypeAnimation
           sequence={[
-            // Same substring at the start will only be typed out once, initially
-            "fast",
-            3500, // wait 1s before replacing "Mice" with "Hamsters"
-            "multithreaded",
-            2500,
-            "async",
-            4000,
+            "Multithreaded",
+            4500,
+            "Customizable",
+            4500,
+            "Powerful",
+            4500,
+            "Fast",
+            4500,
+            "Robust",
+            4500,
+            "Lightweight",
           ]}
           wrapper="span"
-          speed={30}
+          speed={10}
           repeat={Infinity}
           className="text-primary"
-        />{" "}
+          style={{ width: "100%", display: "block" }}
+        />
         server you never knew you needed
       </h1>
       <h2 className="text-white py-[25px] font-roboto text-[20px] tracking-[160%] opacity-65 max-w-[798px] text-center">
